efe5fc0484db31b5f22c94814aac00bd6d6a6d4e
Marcelo Ferreira <contato_marceloferreira98@outlook.com>
2021-04-23 15:09:09 -0300

    Add 'Hello world' and test lines

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,10 @@
+console.log("Hello world") //sentença de código
+
+{
+
+  {
+    console.log("Isso é uma sentença de código") //sentença de código
+
+  } //final do bloco de código 2 (linha 5 até a linha 8)
+
+} //final do bloco de código 1 (linha 3 até a linha 10)
